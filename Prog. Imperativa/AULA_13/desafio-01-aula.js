31085c495d0c9b83db79885eea89a84a15398724
Brenno Machado <brennomachado@tutanota.com>
2021-11-24 02:06:22 -0300

    aula 13 - atividade

---
+++ Prog. Imperativa/AULA_13/desafio-01-aula.js
@@ -0,0 +1,36 @@
+//Desafio 01 - Banco
+
+// let joao = {
+//   num_conta: 01,
+//   tipo_conta: "corrente",
+//   saldo: 1000,
+//   nome: "Joao Cabral",
+// };
+
+function Cliente(conta, tipo_conta, saldo, nome) {
+  this.num_conta = conta;
+  this.tipo_conta = tipo_conta;
+  this.saldo = saldo;
+  this.nome_completo = nome;
+}
+
+let dados = [
+  [5486273622, "Conta Corrente", 27771, "Abigael Natte"],
+  [1183971869, "Conta Poupança", 8675, "Ramon Connell"],
+  [9582019689, "Conta Poupança", 27363, "Jarret Lafuente"],
+  [1761924656, "Conta Poupança", 32415, "Ansel Ardley"],
+  [7401971607, "Conta Poupança", 18789, "Jacki Shurmer"],
+  [630841470, "Conta Corrente", 28776, "Jobi Mawtus"],
+  [4223508636, "Conta Corrente", 2177, "Thomasin Latour"],
+  [185979521, "Conta Poupança", 25994, "Lonnie Verheijden"],
+  [3151956165, "Conta Corrente", 7601, "Alonso Wannan"],
+  [2138105881, "Conta Poupança", 33196, "Bendite Huggett"],
+];
+
+let clientes = [];
+
+for (let i = 0; i < dados.length; i++) {
+  //Continuar daqui com o grupo
+}
+
+console.log(novo_cliente);
