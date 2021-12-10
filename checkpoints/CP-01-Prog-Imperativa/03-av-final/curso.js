a96f9d44b03611a8dabc907772a01abfa6a90ad1
Brenno Machado <brennomachado@tutanota.com>
2021-12-10 00:21:25 -0300

    início passo 3

---
+++ checkpoints/CP-01-Prog-Imperativa/03-av-final/curso.js
@@ -0,0 +1,11 @@
+let Alunos = require("./alunos");
+
+// Teste de importação de módulo
+/* let brenno = new Alunos("Brenno P Machado", 1, [9, 9, 10]);
+brenno.faltas();
+console.log(brenno);
+console.log(brenno.calcularMedia()); */
+
+/* Em um arquivo diferente, crie o objeto literal curso que tem como atributos: nome do curso (string), nota de aprovação (number), faltas máximas (number) e uma lista de estudantes (um array composto pelos alunos criados no passo 1).
+
+ */
